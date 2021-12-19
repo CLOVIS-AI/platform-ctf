d66d36add37a72545bebcebe5dccdd2aeb7f1ea6
Maxime GIRARDET <girardetmaxime@gmail.com>
2021-12-19 19:29:17 +0100

    refactor(web-ui): Removed progressbar.js use in start_challenge function

--- web-ui/js/challenge.js
+++ web-ui/js/challenge.js
@@ -59,59 +59,26 @@ export async function fetchFlagAPI(flag, step_id, onSuccess, onError) {
 }
 
 export async function start_challenge() {
-	document.querySelector("#challenge-status").innerHTML =
-		'<div class="alert alert-primary">The resources of the challenge are currently being provisioned &nbsp;<i class="fa fa-spinner fa-spin"></i></div>'
+	document.querySelector("#challenge-status").innerHTML = '<div class="alert alert-primary">The resources of the challenge are currently being provisioned &nbsp;<i class="fa fa-spinner fa-spin"></i></div>'
 
 	// Progress bar
 	document.querySelector("#progress-bar").hidden = false
-	const bar = new ProgressBar.Path("#progress-bar", {
-		strokeWidth: 2,
-		easing: "linear",
-		duration: start_duration,
-		color: "#78d7fa",
-		trailColor: "#eee",
-		trailWidth: 1,
-		svgStyle: {width: "100%", height: "100%"},
-		from: {color: "#87dfff"},
-		step: (state, bar) => {
-			bar.path.setAttribute("stroke", state.color)
-		}
-	})
-
 	document.querySelector("#start-btn").disabled = true
-	bar.animate(0.9, function () {
-		bar.animate(1, {duration: 20000, easing: "linear"})
-	})
-
 	const onSuccess = data => {
-		if (data.status === "started" && data.challenge === CHALLENGE_ID) {
-			bar.stop()
-			bar.animate(
-				1,
-				{duration: 500, to: {width: 1, color: "#63e047"}},
-				function () {
-					show_instance_status(data)
-					const progress_bar = document.querySelector("#progress-bar")
-					progress_bar.hidden = true
-					progress_bar.innerHTML = ""
-					handle_timer(data.expiration)
-					interval = setInterval(function () {
-						handle_timer(data.expiration)
-					}, 1000)
-				}
-			)
+		if (data.status === "started" && data.challenge === challenge_id) {
+			show_instance_status(data)
+			const progress_bar = document.querySelector("#progress-bar")
+			progress_bar.hidden = true
+			progress_bar.innerHTML = ""
+			handle_timer(data.expiration)
+			interval = setInterval(function () {
+				handle_timer(data.expiration)
+			}, 1000)
 		} else {
 			show_instance_status(data, false)
-			bar.stop()
-			bar.animate(
-				1,
-				{duration: 500, to: {width: 1, color: "#E74C3C"}},
-				function () {
-					const progress_bar = document.querySelector("#progress-bar")
-					progress_bar.hidden = true
-					progress_bar.innerHTML = ""
-				}
-			)
+			const progress_bar = document.querySelector("#progress-bar")
+			progress_bar.hidden = true
+			progress_bar.innerHTML = ""
 		}
 	}
 
@@ -119,21 +86,13 @@ export async function start_challenge() {
 		try {
 			const data = await response.json()
 			if (data.status === "unauthentified") {
-				document.querySelector("#challenge-status").innerHTML =
-					'<div class="alert alert-danger">You need to be authenticated to start a challenge.</div>'
+				document.querySelector("#challenge-status").innerHTML = '<div class="alert alert-danger">You need to be authenticated to start a challenge.</div>'
 			} else {
 				show_instance_status(data, false)
-				bar.stop()
-				bar.animate(
-					1,
-					{duration: 500, to: {width: 1, color: "#E74C3C"}},
-					function () {
-						document.querySelector("#start-btn").disabled = false
-						const progress_bar = document.querySelector("#progress-bar")
-						progress_bar.hidden = true
-						progress_bar.innerHTML = ""
-					}
-				)
+				document.querySelector("#start-btn").disabled = false
+				const progress_bar = document.querySelector("#progress-bar")
+				progress_bar.hidden = true
+				progress_bar.innerHTML = ""
 			}
 		} catch (e) {
 			console.error(e)
