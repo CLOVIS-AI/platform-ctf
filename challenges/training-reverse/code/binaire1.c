652784a96a83b7a1b3faa78fe951d616e6daf8f9
benjamin <bupton@enseirb-matmeca.fr>
2022-01-13 17:30:48 +0100

    ajout du training de reverse

---
+++ challenges/training-reverse/code/binaire1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+This program prints ``Hello world’’
+*/
+int main(int argc, char *argv[])
+{
+
+    char * flag = "the flag is U_C_!ts_eazee";
+    printf("%.*s ... \n", 12, flag);
+    return 0;
+}
---
+++ challenges/training-reverse/code/binaire2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+This program prints ``Hello world’’
+*/
+int main(int argc, char *argv[])
+{
+    printf("Hello world\n");
+    char * flag = "MyF!r5tCPr0gram";
+    return 0;
+}
