c9ba5d2cfb66fe6985eb7c8658f5dd81a2517cfc
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-01-18 10:32:17 +0100

    feat(web): Implemented Terraform test migration

---
+++ automation/test/start.tf
@@ -0,0 +1,55 @@
+# This terraform configuration is used to start the platform on a random free port, so it can be used for tests during review.
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+variable "deployment_user" {
+  type = string
+}
+
+variable "deployment_port" {
+  type = string
+}
+
+variable "deployment_host" {
+  type = string
+}
+
+variable "build_version" {
+  type = string
+}
+
+variable "secrets" {
+  type = set(string)
+}
+
+provider "docker" {
+  host = "ssh://${var.deployment_user}@${var.deployment_host}:${var.deployment_port}"
+}
+
+resource "docker_container" "docker_image" {
+  image = "registry.gitlab.com/rsr22/plateforme-ctf/web:${var.build_version}"
+  name  = "docker_web_interface_${var.build_version}"
+  env = var.secrets
+  ports {
+    internal = 8000
+  }
+  volumes {
+    container_path = "/app/data"
+    volume_name = "ctf-data-${var.build_version}"
+  }
+  volumes {
+    container_path = "/app/logs"
+    volume_name = "ctf-logs-${var.build_version}"
+  }
+}
+
+output "port" {
+  value = docker_container.docker_image.ports[0].external
+}
